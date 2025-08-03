3de667aeb52bc9d928b400c6493af1dc45873a9f
Anunandana <125750906+Anunandana@users.noreply.github.com>
2025-08-03 14:40:16 +0530

    Update Construct Binary Tree from Preorder and Inorder Traversal.py

--- Medium/Construct Binary Tree from Preorder and Inorder Traversal.py
+++ Medium/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -1,1 +1,24 @@
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        # Map from value to its index in inorder traversal
+        inorder_index_map = {val:ind for ind, val in enumerate(inorder)}
+        self.pre_index = 0
+        def helper(in_left, in_right):
+            if in_left > in_right:
+                return None
+            root_val = preorder[self.pre_index]
+            self.pre_index += 1
+            root = TreeNode(root_val)
+            index = inorder_index_map[root_val]
+            root.left = helper(in_left, index-1)
+            root.right = helper(index+1, in_right)
+            return root
+        return helper(0, len(inorder)-1)
